e5bf40812193de9ad0ff3388e591f1c7c28694b2
woojin0708 <95012721+woojin0708@users.noreply.github.com>
2025-06-10 02:31:28 +0900

    commit

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -4,7 +4,6 @@ export default function ProjectDetailPage() {
   return (
     <div className="w-screen h-[calc(100vh-100px)] mx-auto text-left px-4 md:px-8">
       <div>
-        2q314
         <ProjectDetail />
       </div>
     </div>
