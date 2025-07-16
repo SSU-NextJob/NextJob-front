73c62a5abe7f9f28fa3ee21245b7ec6d77ea9987
Gcy_03 <53499057+minhoo03@users.noreply.github.com>
2025-07-17 00:31:47 +0900

    fix: 게시글 상세 조회 id 수정

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,11 +1,14 @@
 import { getPostDetail, type PostDetailResponse } from "@/apis/post";
 import { PostDetailCard } from "@/components/PostDetailCard";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const PostDetail = () => {
+  const { id } = useParams();
   const [postDetail, setPostDetail] = useState<PostDetailResponse>();
   useEffect(() => {
-    getPostDetail(2)
+    if (!id) return;
+    getPostDetail(id)
       .then((res) => {
         if (res.success) setPostDetail(res.data);
       })
