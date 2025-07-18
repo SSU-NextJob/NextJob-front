a844d45df15d2a54e3733cd423a696bdfa1be462
juhyun5060 <juhyun5060@gmail.com>
2025-07-18 23:10:58 +0900

    feat: 알림함 아이콘 추가

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -122,7 +122,9 @@ export const Header = () => {
           className="relative cursor-pointer"
           onClick={() => setIsOpen((prev) => !prev)}
         >
-          <div className="w-8 h-8 bg-gray-200 rounded-full" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+          </svg>
           {unreadCount > 0 && (
             <span className="absolute -top-1 -right-1 bg-red-500 text-white text-[10px] px-1 rounded-full">
               {unreadCount}
