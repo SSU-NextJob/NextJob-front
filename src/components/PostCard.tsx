45a00c799a5e6c4a1e8fd06c809a11af061bf6f1
juhyun5060 <juhyun5060@gmail.com>
2025-07-18 23:38:47 +0900

    feat: PostCard UI 수정

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -33,7 +33,12 @@ export const PostCard = ({ posts }: { posts: PostResponse[] }) => {
             >
               <div className="flex justify-between items-center mb-3 text-sm text-gray-500">
                 <Badge type={post.project.projectType} />
-                <span>{normalizedDate(post.project.startAt)}</span>
+                <span className="flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 mr-1">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                  </svg>
+                  {normalizedDate(post.project.startAt)}
+                </span>
               </div>
 
               <h2 className="text-lg font-semibold text-gray-800 mb-1">
@@ -88,7 +93,12 @@ export const PostCard = ({ posts }: { posts: PostResponse[] }) => {
 
               <div className="flex items-center justify-between mt-auto">
                 <div className="text-sm text-gray-600 flex flex-col gap-1">
-                  <div>🕒 ~ {normalizedDate(post.project.endAt)}</div>
+                  <div className="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 mr-1">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    ~ {normalizedDate(post.project.endAt)}
+                  </div>
                   {/* <div>
                     👥 ({post.participatingCount}/{post.recruitingCount}) 인원
                   </div> */}
