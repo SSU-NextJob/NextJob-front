4dfc57c8ce8f69a7879d26dce465170b8cd61213
juhyun5060 <juhyun5060@gmail.com>
2025-07-18 23:38:47 +0900

    feat: PostCard UI 수정

--- src/components/MyProjectCard.tsx
+++ src/components/MyProjectCard.tsx
@@ -142,8 +142,13 @@ export const MyProjectsCard = ({
                   </span>
                 </div>
                 <div className="text-xs text-gray-500">
-                  🕒 {normalizedDate(post.project.startAt)} ~{" "}
-                  {normalizedDate(post.project.endAt)}
+                  <div className="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 mr-1">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    {normalizedDate(post.project.startAt)} ~{" "}
+                    {normalizedDate(post.project.endAt)}
+                  </div>
                 </div>
                 {/* <div className="text-xs text-gray-500">
                   👥 {project.applicants}명 지원 • ⏱ {project.dDay}일 남음
